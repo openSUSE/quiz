ceec7b74346e08c1f9a000f3415d89a09eaf56a8
Onuralp SEZER <thunderbirdtr@gmail.com>
2025-05-30 23:21:26 +0300

    fix: update static file serving path for data directory

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/data", express.static(path.join(__dirname, "data")));
+app.use("/data", express.static(dataDirPath));
 
 const quizFileDir = fs
   .readdirSync(dataDirPath)
