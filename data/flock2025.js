3857bead7a0048d37f1614968628db72ff84258e
Douglas DeMaio <ddemaio@suse.de>
2025-06-03 16:37:28 +0200

    Update flock2025.js

--- data/flock2025.js
+++ data/flock2025.js
@@ -1,5 +1,5 @@
 const quizData = {
-  title: "openSUSE Flock 2025",
+  title: "Flock 2025",
   subtitle:
     "Don't worry this quiz is mostly educational. No need to know all the answers.",
   submitAnytime: false,
