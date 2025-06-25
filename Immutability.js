a7def225ab43eb6b8d9eefebda1fbbc58c79600e
Luna Jernberg <bittin@reimu.nl>
2025-06-25 16:57:11 +0200

    Swedish Immutability Translation

---
+++ Immutability.js
@@ -0,0 +1,179 @@
+msgid "Immutability"
+msgstr "Ofäränderlighet"
+
+msgid "Let's learn something about our Immutable distros."
+msgstr "Låt oss lära oss något om våra oföränderliga distributioner."
+
+msgid "In simple terms, how does 'transactional-update' powering our Immutable distros work?"
+msgstr "Enkelt uttryckt, hur fungerar "transaktionsuppdatering" som driver våra oföränderliga distributioner?"
+
+msgid "Most system partitions are mounted read-only, preventing changes on disk."
+msgstr "De flesta systempartitioner är monterade skrivskyddade, vilket förhindrar ändringar på disken."
+
+msgid "Updates don’t affect the running system until rebooting into a new snapshot."
+msgstr "Uppdateringar påverkar inte det körande systemet förrän omstart till en ny ögonblicksavbild."
+
+msgid "The system uses transactional-update for safe, atomic upgrades and easy rollback."
+msgstr "Systemet använder transaktionell uppdatering för säkra, atomära uppgraderingar och enkel återställning"
+
+msgid "Which of these are openSUSE Immutable distros?"
+msgstr "Vilka av dessa är OpenSUSE oföränderliga-distributioner?"
+
+msgid "Silverblue, CoreOS"
+msgstr ""
+
+msgid "MicroOS, Leap Micro, Aeon, Kalpa"
+msgstr ""
+
+msgid "Argon, Krypton"
+msgstr ""
+
+msgid "Hurd, Darwin"
+msgstr ""
+
+msgid "What is openSUSE MicroOS?"
+msgstr "Vad är openSUSE MicroOS?"
+
+msgid "A rolling-release web browser"
+msgstr "En webbläsare med rullande släpp"
+
+msgid "Micro Service OS built by the openSUSE community"
+msgstr "Micro Service OS byggt av openSUSE-gemenskapen"
+
+msgid "A package manager"
+msgstr "En pakethanterare"
+
+msgid "A container runtime"
+msgstr "En behållarkörningstid"
+
+msgid "Which desktop environment does openSUSE Aeon use by default?"
+msgstr "Vilken skrivbordsmiljö använder openSUSE Aeon som standard?"
+
+msgid "Enlightenment"
+msgstr ""
+
+msgid "Which desktop environment does openSUSE Kalpa use by default?"
+msgstr "Vilken skrivbordsmiljö använder openSUSE Kalpa som standard?"
+
+msgid "Both are based on MicroOS with a ready-to-use desktop."
+msgstr "Båda är baserade på MicroOS med en färdig skrivbordsmiljö."
+
+msgid "Both use transactional-updates for rpm installation."
+msgstr "Båda använder transaktionella uppdateringar för rpm-installation"
+
+msgid "Both promote distrobox and flatpaks for extra apps."
+msgstr "Båda marknadsför Distrobox och Flatpaks för extra program."
+
+msgid "Both Aeon and Kalpa have interesting names indirectly referring to a rolling release. How so?"
+msgstr "Både Aeon och Kalpa har intressanta namn som indirekt syftar på en rullande utgåva. Hur då?"
+
+msgid "Both are ancient terms meaning long periods or cosmic cycles."
+msgstr "Båda är forntida termer som betyder långa perioder eller kosmiska cykler."
+
+msgid "Both are names of famous Western movies where rolling tumbleweed appeared."
+msgstr "Båda är namn på kända västernfilmer där rullande tumbleweed dök upp."
+
+msgid "Both are very old nicknames of the original architect behind MicroOS."
+msgstr "Båda är mycket gamla smeknamn på den ursprungliga arkitekten bakom MicroOS."
+
+msgid "Both refer to software update tools in openSUSE."
+msgstr "Båda hänvisar till verktyg för programuppdatering i openSUSE."
+
+msgid "There was an experiment with a Sway-based immutable desktop in 2023. Do you know its name?"
+msgstr "Det gjordes ett experiment med ett Sway-baserat, oföränderligt skrivbord år 2023. Vet du vad det heter?"
+
+msgid "Greyhound"
+msgstr ""
+
+msgid "Greybeard"
+msgstr ""
+
+msgid "Bluehair"
+msgstr ""
+
+msgid "Ultraviolet"
+msgstr ""
+
+msgid "Which tool is used to safely update the base system in our Immutable distributions?"
+msgstr "Vilket verktyg används för att säkert uppdatera bassystemet i våra ofrändliga distributioner"
+
+msgid "zypper dup"
+msgstr ""
+
+msgid "transactional-update"
+msgstr ""
+
+msgid "ostree pull"
+msgstr ""
+
+msgid "How can you install an additional package?"
+msgstr "Hur kan du installera ett extra paket?"
+
+msgid "zypper in"
+msgstr ""
+
+msgid "dnf install"
+msgstr ""
+
+msgid "transactional-update pkg install"
+msgstr ""
+
+msgid "rpm-ostree install"
+msgstr ""
+
+msgid "Why is 'transactional-update shell' generally not recommended?"
+msgstr "Varför rekommenderas 'transaktionsuppdateringsskal' generellt sett inte?"
+
+msgid "Because you can modify all parts of the system."
+msgstr "Eftersom du kan modifiera alla delar av systemet."
+
+msgid "It creates wormholes into another dimension."
+msgstr "Det skapar maskhål till en annan dimension"
+
+msgid "It can secretly migrate your system to Fedora."
+msgstr "Det kan i hemlighet migrera ditt system till Fedora."
+
+msgid "Geeko loses a hair every time you use it."
+msgstr "Geeko tappar ett hårstrå varje gång du använder det."
+
+msgid "How do our Immutable distros allow writing to /etc at runtime?"
+msgstr "Hur tillåter våra oföränderliga distributioner skrivning till /etc vid körning?"
+
+msgid "By using overlayfs on top of /etc"
+msgstr "Genom att använda overlayfs ovanpå /etc"
+
+msgid "By mounting a writable Btrfs subvolume directly"
+msgstr "Genom att montera en skrivbar Btrfs-undervolym direkt"
+
+msgid "By disabling read-only mode during boot"
+msgstr "Genom att inaktivera skrivskyddat läge under uppstart"
+
+msgid "Through rpm-ostree layering"
+msgstr "Genom rpm-otree lager"
+
+msgid "Aside from the update mechanism in Immutable distros, what other components SUPPORT transactional-updates?"
+msgstr "Förutom uppdateringsmekanismen i dom oföränderliga distributionerna, vilka andra komponenter STÖDJER transaktionella uppdateringar?"
+
+msgid "Salt and Ansible"
+msgstr "Salt och Ansible"
+
+msgid "cockpit-tukit"
+msgstr ""
+
+msgid "Only one of the following immutable distributions has regular point releases, the rest are rolling. Which one?"
+msgstr "Endast en av följande oföränderliga distributioner har regelbundna punktutgåvor, resten är rullande. Vilken?"
+
+msgid "What bootloader is used by default in openSUSE Aeon?"
+msgstr "Vilken startladdare används som standard i openSUSE Aeon"
+
+msgid "lilo"
+msgstr ""
+
+msgid "BOOTMGR"
+msgstr ""
+
+msgid "grub2"
+msgstr ""
+
+msgid "systemd-boot"
+msgstr ""
