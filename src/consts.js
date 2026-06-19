03a9ad70c96eca784a26dfde31e2896626edc88f
Dan Čermák <dan.cermak@posteo.net>
2026-06-19 11:21:58 +0200

    make the reset_token mandatory

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const path = require("path");
-const fs = require("fs"); // Added fs module
 
 const DATA_DIR_BASEPATH = process.env.LAMBDA_TASK_ROOT
   ? path.join(process.env.LAMBDA_TASK_ROOT, "src")
@@ -8,10 +7,7 @@ const DATA_DIR_BASEPATH = process.env.LAMBDA_TASK_ROOT
 
 const DATA_DIR_PATH = path.join(DATA_DIR_BASEPATH, "..", "data");
 
-// Check for .env file existence in the project root
 const projectRoot = path.join(__dirname, "..");
-const envPath = path.join(projectRoot, ".env");
-const envFileExists = fs.existsSync(envPath);
 
 // STATS_MODE defaults to "STATS_FILE" if not set in .env or if .env doesn't exist.
 const STATS_MODE = process.env.STATS_MODE || "STATS_FILE";
@@ -21,10 +17,7 @@ let resolvedStatsFilePath =
   process.env.STATS_FILE_PATH || path.join(projectRoot, "data", "stats.json");
 const STATS_FILE_PATH = resolvedStatsFilePath;
 
-// RESET_TOKEN defaults to "supersecret" if not set in .env or if .env doesn't exist.
-const RESET_TOKEN = envFileExists
-  ? process.env.RESET_TOKEN || ""
-  : "supersecret";
+const RESET_TOKEN = process.env.RESET_TOKEN;
 
 // controls used logo of the quiz
 const THEME = process.env.THEME || "openSUSE";
